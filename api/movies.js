ffaf4d53a4943dc19938476492bbbd6870a8bd17
ykvictor123 <65426665+ykvictor123@users.noreply.github.com>
2024-11-19 18:01:42 -0500

    Update movies.js

--- api/movies.js
+++ api/movies.js
@@ -1,5 +1,3 @@
-import fetch from "node-fetch";
-
 export default async function handler(req, res) {
   const baseUrl = "https://api.hydrax.net/e7646a082cbb83e67d59d25ea3b0f592/list";
   const allMovies = [];
@@ -12,21 +10,21 @@ export default async function handler(req, res) {
       const data = await response.json();
 
       if (data.status && data.items && data.items.length > 0) {
-        // Agregar las películas de la página actual a la lista completa
+        // Agregar películas a la lista total
         allMovies.push(...data.items);
 
-        // Verificar si hay más páginas
-        if (data.pagination && data.pagination.next) {
+        // Determinar si hay una siguiente página
+        if (data.pagination && data.pagination.next > currentPage) {
           currentPage = data.pagination.next;
         } else {
-          hasNextPage = false; // No hay más páginas
+          hasNextPage = false;
         }
       } else {
-        hasNextPage = false; // Detener si no hay datos
+        hasNextPage = false;
       }
     }
 
-    // Enviar la lista completa de películas
+    // Devolver todas las películas al cliente
     res.status(200).json({ items: allMovies });
   } catch (error) {
     console.error("Error al obtener las películas de Hydrax:", error);
